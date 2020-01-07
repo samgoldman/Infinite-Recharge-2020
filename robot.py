60193a272e81e88793fd55e6c454869658bfee68
gburgess42 <gburgess@cherrycreekschools.org>
2020-01-06 17:59:06 -0700

    Got auto to work

--- robot.py
+++ robot.py
@@ -10,18 +10,26 @@ class MyRobot(wpi.TimedRobot):
         self.leftMotor = wpi.SpeedControllerGroup(ctre.WPI_TalonSRX(7))
         self.rightMotor = wpi.SpeedControllerGroup(ctre.WPI_TalonSRX(22))
         self.drive = wpi.drive.DifferentialDrive(self.leftMotor, self.rightMotor)
+        self.timer = wpilib.Timer()
 
         self.joystick = wpi.XboxController(1)
         
     def teleopPeriodic(self):
         self.drive.arcadeDrive(self.joystick.getY()*3/4, self.joystick.getX()*3/4)
 
-    def autonomusInit(self):
+    def autonomousInit(self):
         self.timer.reset()
         self.timer.start()
 
-    def autonomusPeriodic(self):
-        self.teleopPeriodic()
+    def autonomousPeriodic(self):
+        #self.teleopPeriodic()
+        
+        if self.timer.get() < 2.0:
+            self.drive.arcadeDrive(-0.5, 0)  # Drive forwards at half speed
+        else:
+            self.drive.arcadeDrive(0, 0)  # Stop robot
+        
+
 
 if __name__ == '__main__':
     wpi.run(MyRobot)
