23a3e1f3e10be09ba0651756632312eb47b2daa6
blazerbots <ohsblazerbots@gmail.com>
2020-01-09 18:08:45 -0700

    Added dictionary

---
+++ port.py
@@ -0,0 +1,10 @@
+talonPorts = {
+    # Motors involved in moving the robot around the field
+    "leftChassisMotor": 22,
+    "rightChassisMotor": 7,
+    
+    # Intake Motor which spins the intake mechnaism to grab and store balls
+    "intakeMotor": 21,
+
+    
+}
--- robot.py
+++ robot.py
@@ -3,19 +3,32 @@ import wpilib as wpi
 import wpilib.drive
 from wpilib.interfaces import GenericHID
 
+from networktables import NetworkTables
+from wpilib import PowerDistributionPanel
+
 
 class MyRobot(wpi.TimedRobot):
 
     def robotInit(self):
-        self.leftMotor = wpi.SpeedControllerGroup(ctre.WPI_TalonSRX(7))
+        self.leftMotor = wpi.SpeedControllerGroup(ports.talonPorts.get("liftMotor"))
         self.rightMotor = wpi.SpeedControllerGroup(ctre.WPI_TalonSRX(22))
         self.drive = wpi.drive.DifferentialDrive(self.leftMotor, self.rightMotor)
 
         #self.panelMotor = wpi.SpeedControllerGroup([ctre.WPI_TalonSRX([INSERT MOTOR NUM]))
 
-        #self.shooterMotor = wpi.SpeedControllerGroup([ctre.WPI_TalonSRX([INSERT MOTOR NUM]))
+        #self.liftMotor = wpi.SpeedControllerGroup([ctre.WPI_TalonSRX([INSERT MOTOR NUM]))
 
-        #self.intakeMotor = wpi.SpeedControllerGroup([ctre.WPI_TalonSRX([INSERT MOTOR NUM]))
+        #self.shooterMotorOne = wpi.SpeedControllerGroup([ctre.WPI_TalonSRX([INSERT MOTOR NUM]))
+        #self.shooterMotorTwo = wpi.SpeedControllerGroup([ctre.WPI_TalonSRX([INSERT MOTOR NUM]))
+
+        #self.intakeMotorOne = wpi.SpeedControllerGroup([ctre.WPI_TalonSRX([INSERT MOTOR NUM]))
+        #self.intakeMotorTwo = wpi.SpeedControllerGroup([ctre.WPI_TalonSRX([INSERT MOTOR NUM]))
+
+
+        #self.dashboard = NetworkTables.getTable("Dashboard")
+        #self.pdp = PowerDistributionPanel([PORT NUMBERS]) <----Research PDP class
+
+
 
         self.timer = wpilib.Timer()
 
@@ -24,12 +37,24 @@ class MyRobot(wpi.TimedRobot):
     def teleopPeriodic(self):
         self.drive.arcadeDrive(self.joystick.getY()*3/4, self.joystick.getX()*3/4)
 
+        #self.dashboard.putNumber("RobotSpeed", speed)
+        #self.dashboard.putNumber("RobotRotation", rotation)
+        #self.dashboard.putNumber("PDPCurrent", self.pdp.getTotalCurrent())
+        #self.dashboard.putNumber("PDPTemperature", self.pdp.getTemperature())
+        #self.dashboard.putNumber("PDPVoltage", self.pdp.getVoltage())
+
     def autonomousInit(self):
         self.timer.reset()
         self.timer.start()
 
     def autonomousPeriodic(self):
         #self.teleopPeriodic()
+        
+        #self.dashboard.putNumber("RobotSpeed", speed)
+        #self.dashboard.putNumber("RobotRotation", rotation)
+        #self.dashboard.putNumber("PDPCurrent", self.pdp.getTotalCurrent())
+        #self.dashboard.putNumber("PDPTemperature", self.pdp.getTemperature())
+        #self.dashboard.putNumber("PDPVoltage", self.pdp.getVoltage())
         
         if self.timer.get() < 2.0:
             self.drive.arcadeDrive(-0.5, 0)  # Drive forwards at half speed
