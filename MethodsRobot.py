ca9ba25d16ec3f4916b93f014378bd4f5915df75
blazerbots <ohsblazerbots@gmail.com>
2020-01-23 18:19:36 -0700

    Commited Prompt Change

--- MethodsRobot.py
+++ MethodsRobot.py
@@ -3,6 +3,7 @@ import wpilib as wpi
 from wpilib.drive import DifferentialDrive
 from wpilib.interfaces import GenericHID
 import ports
+from rev.color import ColorSensorV3
 
 class Drive():
     def __init__(self):
@@ -10,6 +11,8 @@ class Drive():
         self.leftMotor = wpi.SpeedControllerGroup(ctre.WPI_TalonSRX(ports.talonPorts.get("leftChassisMotor")))
         self.rightMotor = wpi.SpeedControllerGroup(ctre.WPI_TalonSRX(ports.talonPorts.get("rightChassisMotor")))
         self.drive = wpi.drive.DifferentialDrive(self.leftMotor, self.rightMotor)
+        #self.colorSensor = ColorSensorV3(wpi.I2C.Port.kOnboard)
+
 
     def basicDrive(self, x, y):
         if self.drive is None:
@@ -37,7 +40,7 @@ class Shooter():
             self.bToggle = not self.bToggle
             self.aToggle = False
 
-        if(bPress):
+        if(aPress):
             self.aToggle = not self.aToggle
             self.bToggle = False
 
@@ -52,7 +55,47 @@ class Shooter():
             self.intakeMotor.stopMotor()
 
     def shooting(self, triggerPress):
-        if(triggerPress == 1):
+        if(triggerPress == 1.0):
             self.shootingMotor1.set(.9)
             self.shootingMotor2.set(.9)
+        else:
+            self.shootingMotor1.stopMotor()
+            self.shootingMotor2.stopMotor()
+
+
+class Controller():
+    def __init__(self):
+        self.colorSensor = ColorSensorV3(wpi.I2C.Port.kOnboard)
+        self.controllerMotor = ctre.WPI_TalonSRX(ports.talonPorts.get("controllerMotor"))
+        self.lToggle = False
+        self.rToggle = False
+        self.currentColor = self.colorSensor.getColor() #Variable to determine what the current color sensed by the sensor is
+        self.ir = self.colorSensor.getIR()
+        wpi.SmartDashboard.putNumber("Red", self.currentColor.red)
+        wpi.SmartDashboard.putNumber("Green", self.currentColor.green)
+        wpi.SmartDashboard.putNumber("Blue", self.currentColor.blue)
+        wpi.SmartDashboard.putNumber("IR", self.ir)
+        self.colorProximity = self.colorSensor.getProximity()
+        wpi.SmartDashboard.putNumber("Proximity", self.colorProximity)
+        self.colorCount = None #Variable to count how many times the color has changed, may also be used to count how many times the wheel has been spun
+        self.controllerCheck = False #False: # of spins or on certian color reqs not met, keep going True: conditions met, stop
         
+
+    def spinner(self, lPress, rPress):
+        if(lPress):
+            self.lToggle = not self.lToggle
+            self.rToggle = False
+
+        if(rPress):
+            self.rToggle = not self.rToggle
+            self.lToggle = False
+
+        #Rotate backwards for a short time and stop. Acts as a way to quickly reverse slightly
+        if(self.lToggle):
+            pass
+
+        #Keep spinning CW until toggled off by pressing the same bumper a second time
+        if(self.rToggle and self.controllerCheck):
+            self.controllerMotor.set(.1)
+        else:
+            self.controllerMotor.stopMotor()
--- ports.py
+++ ports.py
@@ -1,14 +1,23 @@
 talonPorts = {
     # Motors involved in moving the robot around the field
-    "leftChassisMotor": 7,
-    "rightChassisMotor": 22,
+    "leftChassisMotor": 11, #7
+    "rightChassisMotor": 12, #22
     
     # Intake Motor which spins the intake mechnaism to grab and store balls
     "intakeMotor": 10,
 
+<<<<<<< HEAD
     # Shooting Motor which spins the shooting mechnaism to shoot balls out of the robot storage
+    "shootingMotor1": 7, #11
+    "shootingMotor2": 22, #11
+=======
+    # Shooting Motor which spins the shooting mechanism to shoot balls out of the robot storage
     "shootingMotor1": 11,
     "shootingMotor2": 12,
+
+    #Motor for the lazy susan used to spin the controller (spinny rainbow wheel)
+    "controllerMotor": 13,
+>>>>>>> 48a1e47f1db7056bab92da8c62569607894a1212
 }
 
 controllerPorts = {
